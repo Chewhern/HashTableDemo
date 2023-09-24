33fff93e95b97f790e796cc932dc37b811638ede
Chrono <chrono_legionnaire@protonmail.com>
2023-09-24 15:37:27 +0800

    Add files via upload

---
+++ Source/0.01/Helper/HashTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo.Helper
+{
+    public class HashTable
+    {
+        public HashTableElement[] myHashTableElements = new HashTableElement[] { };
+        public List<int> UsedIndex = new List<int>();
+    }
+}
---
+++ Source/0.01/Helper/HashTableElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo.Helper
+{
+    public class HashTableElement
+    {
+        public int Value;
+        public List<int> ChainedValues = new List<int>();
+        public int State;
+    }
+}
